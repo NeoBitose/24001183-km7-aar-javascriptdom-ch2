bcb4c0d3f77d5024182994c3399e787cca3d122d
NeoBitose <ramadhanalif430@gmail.com>
2024-09-26 22:53:16 +0700

    feat: add filtering logic in filtering cars page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,39 @@
 class App {
     constructor() {
-      this.clearButton = document.getElementById("clear-btn");
-      this.loadButton = document.getElementById("load-btn");
-      this.carContainerElement = document.getElementById("cars-container");
-      this.driverType = document.getElementById("driver-type");
-      this.dateRent = document.getElementById("date-rent");
-      this.timeRent = document.getElementById("time-rent");
-      this.passanger = document.getElementById("passanger");
+        this.clearButton = document.getElementById("clear-btn");
+        this.loadButton = document.getElementById("load-btn");
+        this.carContainerElement = document.getElementById("cars-container");
+        this.driverType = document.getElementById("driver-type");
+        this.dateRent = document.getElementById("date-rent");
+        this.timeRent = document.getElementById("time-rent");
+        this.passanger = document.getElementById("passanger");
     }
-  
+
     async init() {
-      await this.load();
-      this.run();
-      // Register click listener
-    //   this.clearButton.onclick = this.clear;
-    //   this.loadButton.onclick = this.run;
+        await this.loadFilter();
+        console.log("ok");
+        // this.run();
+        // this.loadButton.onclick = this.run;
+        this.loadButton.onclick = async () => {
+            this.clear(); 
+            await this.loadFilter();
+            this.run();
+        };
     }
-  
+
     run = () => {
-      Car.list.forEach((car) => {
-        const node = document.createElement("div");
-        node.classList.add("col-lg-4");
-        node.innerHTML = car.render();
-        this.carContainerElement.appendChild(node);
-      });
+        Car.list.forEach((car) => {
+            const node = document.createElement("div");
+            node.classList.add("col-lg-4");
+            node.innerHTML = car.render();
+            this.carContainerElement.appendChild(node);
+        });
     };
-  
+
     async load() {
-      const cars = await Binar.listCars();
-      Car.init(cars);
-      console.log(cars)
+        const cars = await Binar.listCars();
+        Car.init(cars);
+        console.log(cars)
     }
 
     async loadFilter() {
@@ -43,26 +47,21 @@ class App {
             const notAvailableAt = (this.driverType.value === 'false' && !data.available ? true : false)
             const passangers = data.capacity >= this.passanger.value
 
-            if (this.driverType.value !== 'default' && this.dateRent.value !== '' && this.timeRent.value !== 'false' && this.passanger.value >= 0) 
-            {
+            if (this.driverType.value !== 'default' && this.dateRent.value !== '' && this.timeRent.value !== 'false' && this.passanger.value >= 0) {
                 return (availableAt || notAvailableAt) && checkTime && passangers
-            } 
-            else if (this.driverType.value !== 'default' && this.passanger.value > 0) 
-            {
+            }
+            else if (this.driverType.value !== 'default' && this.passanger.value > 0) {
                 return (availableAt || notAvailableAt) && passangers
-            } 
-            else if (this.dateRent.value !== '' && this.timeRent.value !== 'false' && this.passanger.value > 0) 
-            {
+            }
+            else if (this.dateRent.value !== '' && this.timeRent.value !== 'false' && this.passanger.value > 0) {
                 return checkTime && passangers
-            } 
-            else if (this.dateRent.value !== '' && this.timeRent.value !== 'false') 
-            {
+            }
+            else if (this.dateRent.value !== '' && this.timeRent.value !== 'false') {
                 return checkTime
-            } 
-            else if (this.driverType.value !== 'default') 
-            {
+            }
+            else if (this.driverType.value !== 'default') {
                 return (availableAt || notAvailableAt)
-            } 
+            }
             else {
                 return passangers
             }
@@ -71,14 +70,13 @@ class App {
         console.log(cars)
         Car.init(cars);
     }
-  
+
     clear = () => {
-      let child = this.carContainerElement.firstElementChild;
-  
-      while (child) {
-        child.remove();
-        child = this.carContainerElement.firstElementChild;
-      }
+        let child = this.carContainerElement.firstElementChild;
+
+        while (child) {
+            child.remove();
+            child = this.carContainerElement.firstElementChild;
+        }
     };
-  }
-  
+}
